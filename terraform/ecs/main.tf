06a147ce1bb32c104090734312e96fbf9446f798
lucmaeda <luc.maeda@snyk.io>
2022-07-07 16:22:42 +0900

    minor adjustments

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -61,7 +61,7 @@ resource "aws_iam_instance_profile" "ecs_agent" {
 
 # AWS Auto Scale Launch Configuration
 resource "aws_launch_configuration" "app" {
-  name_prefix = "app-amd-"
+  name_prefix = "app-amd"
   security_groups = [
     aws_security_group.app-amd-22.id,
     aws_security_group.app-amd-80.id,
--- terraform/ecs/networking.tf
+++ terraform/ecs/networking.tf
@@ -27,10 +27,10 @@ resource "aws_subnet" "pub_subnet_a" {
 resource "aws_subnet" "pub_subnet_b" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1b"
+  availability_zone = "ap-northeast-1c"
 
   tags = {
-    Name  = "subnet-northeast1-b",
+    Name  = "subnet-northeast1-c",
     team  = "CustomerSolutions",
     owner = "Snyk TSM APJ"
   }
