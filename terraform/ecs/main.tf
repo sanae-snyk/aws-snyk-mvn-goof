97a0ad0a10160937e913b2c28e813d9212aa3143
lucmaeda <luc.maeda@snyk.io>
2022-07-07 18:16:19 +0900

    minor updates

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -62,7 +62,6 @@ resource "aws_iam_instance_profile" "ecs_agent" {
 # AWS Auto Scale Launch Configuration
 resource "aws_launch_configuration" "app" {
   name        = "app-amd"
-  name_prefix = "app-amd-"
   security_groups = [
     aws_security_group.app-amd-22.id,
     aws_security_group.app-amd-80.id,
